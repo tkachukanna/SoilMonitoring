c08ecb4558b8b0fe02715901a4f61d744306de7c
tkachukanna <tkachukanne@gmail.com>
2022-12-28 08:40:20 +0200

    Added Entities to DAL

---
+++ SoilMonitoring/DAL/Entities/Area.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DAL.Entities
+{
+    class Area
+    {
+        public int IdArea { get; set; }
+        public string Type { get; set; }
+        public string Condition { get; set; }
+        public int IdSoilMonitoring { get; set; }
+        public SoilMonitoring SoilMonitoring { get; set; }
+
+    }
+}
