986f523ba5a3467c5f49b6ff99664e7a6d6bb4d3
tkachukanna <tkachukanne@gmail.com>
2022-12-28 11:27:13 +0200

    Added DAL.Tests.TestAreaRepository and added Moq

---
+++ SoilMonitoring/DAL.Tests/TestAreaRepository.cs
@@ -0,0 +1,17 @@
+﻿using DAL.Entities;
+using DAL.Repositories.Impl;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DAL.Tests
+{
+    class TestAreaRepository : BaseRepository<Area>
+    {
+        public TestAreaRepository(DbContext context) : base(context)
+        {
+
+        }
+    }
+}
